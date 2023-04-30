679b176b0963373f7c87539bd1f0b790a4819472
Francisco Viana <franciscoviana1510@gmail.com>
2023-04-30 12:11:59 -0300

    aula-60

--- App.js
+++ App.js
@@ -1,20 +1,122 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import React, { useState, useEffect } from "react";
+import { View } from "react-native";
+import {
+  DefaultTheme,
+  Provider as PaperProvider,
+  ActivityIndicator,
+  MD2Colors,
+} from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import "firebase/auth";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import rootReducers from "./redux/reducers";
+import thunk from "redux-thunk";
+
+import HeaderBar from "../../components/HeaderBar";
+import Landing from "../../components/auth/Landing";
+import Register from "../../components/auth/Register";
+import Login from "../../components/auth/Login";
+import Main from "../../components/Main";
+import Add from "../../components/main/Add";
+import Save from "../../components/main/Save";
+import Comment from "../../components/main/Comment";
+
+import { app } from "../firebaseConfig";
+
+const Stack = createNativeStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  roundness: 2,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: "#ff7300",
+    accent: "#ffffff",
+  },
+};
+
+const store = createStore(
+  rootReducers,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const auth = getAuth(app);
+    onAuthStateChanged(auth, (user) => {
+      if (!user) {
+        setIsLoggedIn(false);
+      } else {
+        setIsLoggedIn(true);
+      }
+      setIsLoading(false);
+    });
+  }, []);
+
+  const Loading = () => (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <ActivityIndicator
+        animating={true}
+        color={MD2Colors.orange500}
+        size="large"
+      />
     </View>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
+  const LoggedOut = () => (
+    <PaperProvider theme={theme}>
+      <NavigationContainer theme={theme}>
+        <Stack.Navigator initialRouteName="Landing">
+          <Stack.Screen
+            name="Landing"
+            component={Landing}
+            options={{ headerShown: false }}
+          />
+          <Stack.Screen name="Register" component={Register} />
+          <Stack.Screen name="Login" component={Login} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>
+  );
+
+  const LoggedIn = () => (
+    <Provider store={store}>
+      <PaperProvider theme={theme}>
+        <NavigationContainer theme={theme}>
+          <Stack.Navigator initialRouteName="Main">
+            <Stack.Screen
+              name="Main"
+              component={Main}
+              options={{ header: (props) => <HeaderBar {...props} /> }}
+            />
+            <Stack.Screen name="Add" component={Add} />
+            <Stack.Screen name="Save" component={Save} />
+            <Stack.Screen name="Comment" component={Comment} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PaperProvider>
+    </Provider>
+  );
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (isLoggedIn) {
+    return <LoggedIn />;
+  }
+
+  return <LoggedOut />;
+};
+
+export default App;
---
+++ database/firebaseConfig.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCrzGNEKJdpwps8T8zEGVbee90kpWlf_rE",
+  authDomain: "imagigram-d632e.firebaseapp.com",
+  projectId: "imagigram-d632e",
+  storageBucket: "imagigram-d632e.appspot.com",
+  messagingSenderId: "358549700510",
+  appId: "1:358549700510:web:7c7a8cbe67255fc510208f",
+};
+
+export const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore(app);
